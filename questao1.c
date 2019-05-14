33eb54fe4bb09f6459cd3422b23814f5960d5c37
anarcoqr <anarcoqr@gmail.com>
2019-05-14 17:31:34 -0300

    mutex criado porém não funcionando devidamente

--- questao1.c
+++ questao1.c
@@ -3,28 +3,29 @@
 #include <pthread.h>
 
 long int counter = 0;
+pthread_t mutex;
 
 void* run (void* args){
-    int my_id;
+    int my_id = (int) args;
     long int j;
 
-    my_id = (int) args;
-
     for(j = 0;j < 1e7; j++){
+        pthread_mutex_lock(&mutex);
         counter = counter + 1;
+        pthread_mutex_unlock(&mutex);
     }
-
+    
     printf("my_id=%d j=%ld counter=%ld\n", my_id,j, counter);
-    pthread_exit(my_id);
 }
 
 int main (int argc, char *argv[]){
 
     int i;
     pthread_t pthreads[3];
+    pthread_mutex_init(mutex, NULL);
 
     for (i = 0; i < 3; i++){
-        pthread_create(&pthreads[i], NULL, &run, (void*) i);
+        pthread_create(pthreads[i], NULL, &run, (void*) i);
     }
 
     for(i = 0; i < 3; i++){
