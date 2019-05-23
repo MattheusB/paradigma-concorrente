7a4ee75e3113c68d31cc6dd2887d184a7aefe734
MattheusB <mattheus.rodrigues@ccc.ufcg.edu.br>
2019-05-23 20:37:07 -0300

    Aparentemente o put do listComparator tá ok, começando o get

--- questao5/ListComparator.java
+++ questao5/ListComparator.java
@@ -7,19 +7,10 @@ import java.util.concurrent.CopyOnWriteArrayList;
 public class ListComparator implements Runnable{
 
 	private int size;
-	private CopyOnWriteArrayList<Integer> copyList;
-	private ArrayList<Integer> synchList;
-	private long startTime;
-	private long stopTime;
-	private long realTime;
 	
     public ListComparator(int numberThreads){
-    	this.copyList = new CopyOnWriteArrayList<>();
-    	this.synchList = new ArrayList<>();
+    	
         this.size = 150000/numberThreads;
-        this.startTime = 0;
-        this.stopTime = 0;
-        this.realTime = 0;
     	
     	
     }
@@ -33,19 +24,67 @@ public class ListComparator implements Runnable{
 	
 	private long putCopyOnWriteArrayList () {
 		
-		this.startTime = System.currentTimeMillis();
-		testPut(this.copyList);
-		this.stopTime = System.currentTimeMillis();
-		this.realTime = this.stopTime - this.startTime;
+		CopyOnWriteArrayList<Integer> copyList = new CopyOnWriteArrayList<>();
+		
+		long startTime = 0;
+		long endTime = 0;
+		long realTime = 0;
+		
+		startTime = System.currentTimeMillis();
+		testPut(copyList);
+		endTime = System.currentTimeMillis();
+		realTime = endTime - startTime;
 		
 		return realTime;
 		
 		
 	}
 	
+	private long putSychronizedList() {
+		ArrayList<Integer> synchList = new ArrayList<>();
+		long startTime = 0;
+		long endTime = 0;
+		long realTime = 0;
+		
+		startTime = System.currentTimeMillis();
+		testPut(synchList);
+		endTime = System.currentTimeMillis();
+		realTime = endTime - startTime;
+		
+		return realTime;
+		
+	}
+	
 	private void testPut(List auxList) {
 		for (int i = 0; i < this.size; i++) {
 			auxList.add(i);
+		}
+	}
+	
+	
+	private long getCopyOnWriteArrayList() {
+		
+		CopyOnWriteArrayList<Integer> copyList = new CopyOnWriteArrayList<>();
+		long startTime = 0;
+		long endTime = 0;
+		long realTime = 0;
+		
+		testPut(copyList);
+		
+		startTime = System.currentTimeMillis();
+		testGet(copyList);
+		endTime = System.currentTimeMillis();
+		realTime = endTime - startTime;
+		
+		return realTime;
+		
+	}
+	
+	
+	private void testGet(List auxList) {
+		
+		for (int i = 0; i < this.size; i++) {
+			auxList.get(i);
 		}
 	}
 
