dfa501936db6003e9fd7adc26d0cb717339f2491
anarcoqr <anarcoqr@gmail.com>
2019-05-28 12:44:44 -0300

    questao 1 funcionando

--- questao1/questao1.c
+++ questao1/questao1.c
@@ -3,32 +3,36 @@
 #include <pthread.h>
 
 long int counter = 0;
-pthread_t mutex;
+pthread_mutex_t mutex;
 
 void* run (void* args){
-    int my_id = (int) args;
+    int my_id;
     long int j;
 
+    my_id = (int) args;
+
+    pthread_mutex_lock(&mutex);
     for(j = 0;j < 1e7; j++){
-        pthread_mutex_lock(&mutex);
         counter = counter + 1;
-        pthread_mutex_unlock(&mutex);
     }
+    pthread_mutex_unlock(&mutex);
     
     printf("my_id=%d j=%ld counter=%ld\n", my_id,j, counter);
+    pthread_exit(my_id);
 }
 
 int main (int argc, char *argv[]){
 
     int i;
     pthread_t pthreads[3];
-    pthread_mutex_init(mutex, NULL);
+    pthread_mutex_init(&mutex, NULL);
 
     for (i = 0; i < 3; i++){
-        pthread_create(pthreads[i], NULL, &run, (void*) i);
+        pthread_create(&pthreads[i], NULL, &run, (void*) i);
     }
 
     for(i = 0; i < 3; i++){
-        pthread_join(pthreads[i], counter);
+        pthread_join(pthreads[i], NULL);
     }
+    printf("counter = %ld\n",counter);
 }
